c06801a40fd0c3dc440376c5a37907eee9a72229
mejdi hafiane <mejdiclubiste@gmail.com>
2020-02-11 17:24:03 +0100

    first commit

---
+++ app/src/main/java/asm/asmtunis/com/bookswipe/fragment/FirstFragment.kt
@@ -0,0 +1,23 @@
+package asm.asmtunis.com.bookswipe.fragment
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import asm.asmtunis.com.bookswipe.R
+
+
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+
+}
